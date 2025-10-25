cdcae0b526d4f8c4dac1f088f7f286285e93c778
rodrigo.sayas <rodrigosayas24@gmail.com>
2025-10-24 21:12:23 -0300

    Dark and Light mode palette

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-  // 1. Importación Correcta: El nombre del archivo debe terminar en .svelte
-  //    Asumo que tu componente se llama TaskListPage.svelte, no taskListFeature.
-  import TaskListPage from "../features/tasks/taskListFeature.svelte"; 
-  
-  // 2. La API de Tauri se importa, pero no se usa en este componente raíz
-  //    (Generalmente, solo se usa en la capa API/Store).
   import { invoke } from "@tauri-apps/api/core";
 
 </script>
 
-<main>
-  <TaskListPage />
+<main class="container">
+
 </main>
